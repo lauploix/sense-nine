6b80526f1d807a62d9660d17939acedba05dd12c
François Guillot <francois@gradle.com>
2025-11-03 16:43:22 +0100

    Add test that fails around 10%

---
+++ com.mechanitis.demo.sense.twitter/src/test/java/com/mechanitis/demo/sense/twitter/BatchSchedulerTest.java
@@ -0,0 +1,24 @@
+package com.mechanitis.demo.sense.twitter;
+
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BatchSchedulerTest {
+
+    @RepeatedTest(value = 20)
+    void testJobRunsOutsideDangerZone() throws InterruptedException {
+        // Get the current time's millisecond of the second
+        int milliOfSecond = LocalTime.now().getNano() / 1000000;
+        Thread.sleep(100);
+
+        // This test will fail if it happens to run in the
+        // last 100ms of a second (e.g., at 12:34:56.912)
+        // This creates a 10% random failure rate.
+        assertTrue(milliOfSecond < 900,
+                   "Flaky failure! Job tried to run in the 'danger zone' at "
+                           + milliOfSecond + "ms.");
+    }
+}
